ab5c25471a2121e5174d8303fe83a578b70e73da
ealitten <ea.litten@gmail.com>
2018-01-08 14:28:20 +0000

    Add top_up tests & method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,8 +2,12 @@ class Oystercard
 
   attr_reader :balance
 
-  def initialize
-    @balance = 0
+  def initialize(initial_balance=0)
+    @balance = initial_balance
+  end
+
+  def top_up(value)
+    @balance += value
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,9 +4,23 @@ describe Oystercard do
 
   subject(:oystercard) { described_class.new }
 
-  it "should return balance" do
-    expect(subject.balance).to eq 0
-  end
+  describe "#balance" do
+
+    it "should return balance" do
+        expect(oystercard.balance).to eq 0
+    end
+
+end
+
+describe "#top_up" do
+
+    it "should increase balance" do
+        expect(oystercard.top_up(3.50)).to eq 3.50
+    end
+
+
+end
+
 
 
 
