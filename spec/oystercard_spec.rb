3d317962c17023615c3953fcc8d251a3520911d3
Ed Perkins <ed@edperkins.co.uk>
2018-01-08 14:21:15 +0000

    add oystercard balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,13 @@
+require "oystercard"
+
 describe Oystercard do
+
+  subject(:oystercard) { described_class.new }
+
+  it "should return balance" do
+    expect(subject.balance).to eq 0
+  end
+
+
+
 end
